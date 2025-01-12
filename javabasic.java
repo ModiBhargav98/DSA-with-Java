1f6d8db10dab67704a4dc1c2a56ab90f8c967279
bhargav.modi <bhargav.modi@radixweb.com>
2025-01-12 18:28:13 +0530

    commit the java basic syntax and how to run java  program

---
+++ javabasic.java
@@ -0,0 +1,18 @@
+// Q1 How is my code running? 
+
+// i compilation
+// ans - source code we have save in .java file and jdk compiler convert into byte code 
+//       java we have a using any operating System only JDK kit is required for run the java
+
+// ii execution
+// ans - byte code go to jvm and jam to native code 
+// machine only for under stand binary value like 0 and 1
+// jvm means java virtual machine
+
+// first run the our class FirstClass after that run the main function in execution time
+class JavaBasic {
+    // main is function name and void is return empty return type
+    public static void main(String[] args) {
+        System.out.println("hello world 10");
+    }
+}
