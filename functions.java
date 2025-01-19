e82290a9d3dc6ef51edde0741e5870284717c214
ModiBhargav98 <modibhargav1998@gmail.com>
2025-01-19 18:01:22 +0530

    functions called in java

---
+++ functions.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Functions {
+
+    // Note: function name always write in camel cases
+    // function and methods both are difference we learn oops with java
+    public static int printMyName(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // main is function name and void is return empty return type
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(printMyName(a, b));
+    }
+}
